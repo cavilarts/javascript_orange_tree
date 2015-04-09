40c362edfff63a15615ba1b58aaa5c6ebfc3647d
juanfrc <juanfrc2@gmail.com>
2015-04-09 11:05:51 -0500

    grow and dead tree

--- javascripts/src/view.js
+++ javascripts/src/view.js
@@ -16,5 +16,13 @@ View.prototype = {
 	  $("#orange-tree-template").css({'display':'block'});
 	  $("#tree").removeClass("display-tree-big");
 	  $("#tree").addClass("display-tree-small");
-	}
+	},
+	growTree: function(){
+	  $("#tree").removeClass("display-tree-small");
+	  $("#tree").addClass("display-tree-big");
+	},
+  deadTree: function(){
+    $("#orange-tree-template").css({'display':'none'});
+    alert("Your tree is dead")    
+  }
 };
