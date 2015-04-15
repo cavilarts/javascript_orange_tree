0eb06c5fc35a1919dde3041b275527e73eca50c5
German Hernandez <muygrafico@gmail.com>
2015-04-15 12:32:40 -0500

    release 2

--- javascripts/src/typed-objects.js
+++ javascripts/src/typed-objects.js
@@ -1,1 +1,43 @@
-// Write your Orange Tree code here - you may use constructor functions 
+FRUIT_BEARING_AGE = 2;
+MAX_AGE = 5;
+AGING_RATE = 1;
+SIZING_RATE = 10;
+
+var OrangeTree = function(){
+  this.height = 0;
+  this.age = 0;
+  this.orangeCount = 0;
+  this.oranges = [];
+  this.diameter = 0;
+  this.isAlive = true;
+};
+OrangeTree.prototype = {
+  grow: function(){
+    this.age += AGING_RATE;
+    this.height += SIZING_RATE;
+    if( this.age == FRUIT_BEARING_AGE ){
+      this.orangeCount = random(20);
+    };
+    if (this.age <= MAX_AGE){
+      this.isAlive = true;
+    }else{
+      this.isAlive = false;
+    }
+  },
+  dropOrange: function(){
+    var droppedOrange = this.orangeCount --;
+    return droppedOrange;
+  },
+  pickOrange: function(){
+    return new orange;
+  },
+  random: function(number){
+    return (Math.ceil(Math.random() * number))
+  }
+}
+var germanOrangeTree = new OrangeTree();
+console.log(germanOrangeTree.age);
+germanOrangeTree.grow();
+germanOrangeTree.grow();
+germanOrangeTree.grow();
+console.log(germanOrangeTree.orangeCount);
